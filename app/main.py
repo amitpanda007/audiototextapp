04952dd5d1fbaca8fc61d9e31d784d060d838aff
Panda <amitkumar.panda@in.imshealth.com>
2022-11-10 17:27:45 +0530

    added feature upload and transcribe

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
 
 from .library.helpers import *
 from app.routers import twoforms, unsplash, accordion
@@ -9,9 +10,9 @@ from app.routers import twoforms, unsplash, accordion
 from fastapi import FastAPI, File, UploadFile
 import aiofiles
 
+import whisper
 
 app = FastAPI()
-
 
 templates = Jinja2Templates(directory="templates")
 
@@ -31,14 +32,65 @@ async def home(request: Request):
 
 @app.get("/page/{page_name}", response_class=HTMLResponse)
 async def show_page(request: Request, page_name: str):
-    data = openfile(page_name+".html")
+    data = openfile(page_name + ".html")
     title = page_name.title() + " - "
     return templates.TemplateResponse("page.html", {"request": request, "data": data, "title": title})
 
+
 @app.post("/upload/")
 async def create_upload_files(file: UploadFile):
-    async with aiofiles.open(f'upload/{file.filename}', 'wb') as out_file:
-        while content := await file.read(1024):  # async read chunk
-            await out_file.write(content)  # async write chunk
+    try:
+        async with aiofiles.open(f'upload/{file.filename}', 'wb') as out_file:
+            while content := await file.read(1024):  # async read chunk
+                await out_file.write(content)  # async write chunk
+        return JSONResponse(
+            status_code=200,
+            content={"message": f"File uploaded successfully."},
+        )
+    except Exception:
+        return JSONResponse(
+            status_code=500,
+            content={"message": f"Oops! Something went wrong..."},
+        )
 
-    return {"Result": "OK"}
+
+@app.post("/transcribe-upload/")
+async def transcribe_upload_files(file: UploadFile):
+    file_mask = ["mp3", "mp4", "mkv", "m4a", "wav", "flac"]
+    filename = file.filename
+    file_ext = filename.rsplit(".", 1)[1]
+    if file_ext.lower() in file_mask:
+        try:
+            async with aiofiles.open(f'upload/{file.filename}', 'wb') as out_file:
+                print("Starting file upload")
+                while content := await file.read(1024):  # async read chunk
+                    await out_file.write(content)  # async write chunk
+
+            print("File upload done. starting transcription.")
+
+            # Start Transcribe with Whisper
+            model = whisper.load_model('medium')
+            # To run only on CPU set fp16=False. eg. model.transcribe(f'./input/{filename}', fp16=False)
+            out = model.transcribe(f'./upload/{filename}',  fp16=False)
+            transcribed_text = out["text"]
+            print(transcribed_text)
+
+            # Write model output to storage for future usage.
+            # with open(f'./output/{name}.json', 'w', encoding='utf-8') as f:
+            #     json.dump(out, f, ensure_ascii=False, indent=4)
+
+            return JSONResponse(
+                status_code=200,
+                content={"message": f"Transcription Completed.", "transcription": transcribed_text},
+            )
+        except Exception:
+            return JSONResponse(
+                status_code=500,
+                content={"message": f"Oops! Something went wrong..."},
+            )
+    else:
+        return JSONResponse(
+            status_code=403,
+            content={"message": f"Unsupported filetype uploaded. Please upload {file_mask} files."},
+        )
+
--- static/js/index.js
+++ static/js/index.js
@@ -7,9 +7,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
         console.log(e);
     });
 
-    const dropZone = document.querySelector("#dropzone");
-    let dropArea = document.getElementById('dropzone');
-    console.log(dropArea);
+    const dropArea = document.getElementById('dropzone');
+    const transcribeText = document.getElementById('transcribe-text');
+    const loadingButton = document.getElementById('loading');
+
 
     ['dragenter', 'dragover', 'dragleave'].forEach(eventName => {
       dropArea.addEventListener(eventName, preventDefaults, false)
@@ -23,6 +24,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     function handleDrop(e) {
       preventDefaults(e);
+
+      dropArea.classList.add('disable-click');
+      loadingButton.classList.add('show');
+
       let dt = e.dataTransfer;
       let files = dt.files;
 
@@ -30,26 +35,88 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
 
     function handleFiles(files) {
-      ([...files]).forEach(uploadFile);
+//      ([...files]).forEach(uploadFile);
+      ([...files]).forEach(uploadFileWithFetch);
     }
 
-    function uploadFile(file) {
-      let url = '/upload';
-      const xhr = new XMLHttpRequest()
-      const formData = new FormData()
-      xhr.open('POST', url, true)
+//    function uploadFile(file) {
+//      let url = '/upload';
+//      const xhr = new XMLHttpRequest();
+//      const formData = new FormData();
+//      xhr.open('POST', url, true);
+//
+//      xhr.addEventListener('readystatechange', function(e) {
+//        console.log(xhr);
+//        if (xhr.readyState == 4 && xhr.status == 200) {
+//          // Done. Inform the user
+//          showToast("success", xhr.responseText.message);
+//        }
+//        else if (xhr.readyState == 4 && xhr.status != 200) {
+//          // Error. Inform the user
+//          showToast("warning", xhr.responseText.detail);
+//        }
+//      })
+//
+//      formData.append('file', file)
+//      xhr.send(formData)
+//    }
 
-      xhr.addEventListener('readystatechange', function(e) {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-          // Done. Inform the user
-        }
-        else if (xhr.readyState == 4 && xhr.status != 200) {
-          // Error. Inform the user
-        }
-      })
 
-      formData.append('file', file)
-      xhr.send(formData)
+
+    function uploadFileWithFetch(file) {
+        const formData = new FormData();
+        const fileField = document.querySelector('input[type="file"]');
+
+        formData.append('file', file);
+
+        fetch('/transcribe-upload', {
+          method: 'post',
+          body: formData
+        })
+          .then((response) => {
+            console.log(response);
+            if(response.ok) {
+                return response.json();
+            }
+            return Promise.reject(response);
+          })
+          .then((result) => {
+            showToast("success", result.message);
+            transcribeText.innerHTML = result.transcription;
+            dropArea.classList.remove('disable-click');
+            loadingButton.classList.remove('show');
+          })
+          .catch((response) => {
+            response.json().then((json) => {
+                showToast("warning", json.message);
+            })
+          });
+    }
+
+    let successTimeout;
+    let warningTimeout;
+    function showToast(type, msg) {
+        if(type == "success") {
+            window.clearTimeout(successTimeout);
+            let successToastSection = document.getElementById('toast-success');
+            let successToastMsg = document.getElementById('toast-success-message');
+            successToastSection.classList.add('show');
+            successToastMsg.innerHTML = msg;
+
+            successTimeout = setTimeout(() => {
+                successToastSection.classList.remove('show');
+            }, 3000);
+        }else if(type == "warning") {
+            window.clearTimeout(warningTimeout);
+            let warningToastSection = document.getElementById('toast-warning');
+            let warningToastMsg = document.getElementById('toast-warning-message');
+            warningToastSection.classList.add('show');
+            warningToastMsg.innerHTML = msg;
+
+            warningTimeout = setTimeout(() => {
+                warningToastSection.classList.remove('show');
+            }, 3000);
+        }
     }
 
 });
